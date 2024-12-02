86563a667c35ef216d39cea4b8687a3f0512ee01
Melissa Schultz <melissaschultz@northwesternmutual.com>
2024-12-02 16:30:07 -0600

    day one coded

--- day1/index.js
+++ day1/index.js
@@ -4,12 +4,33 @@ const {INPUT, TEST_DATA_PART_ONE, TEST_DATA_PART_TWO } = require('../input-types
 
 
 function getInputData (file) {
-  return fs.readSync(path.resolve(__dirname, `./${file}`), 'utf-8').toString().split('\n')
+  return fs.readFileSync(path.resolve(__dirname, `./${file}`), 'utf-8').toString().split('\n')
 }
 
-
 exports.partOne = function (file) {
-  const input = this.getInputData(file)
+  const input = getInputData(file)
+  const rightArray = input.map((line) => {
+    const [left, right] = line.split('  ')
+    return Number(right)
+  }).sort((a, b) => a - b)
+  const leftArray = input.map((line) => {
+    const [left, right] = line.split('  ')
+    return Number(left)
+  }).sort((a, b) => a - b)
+
+  // compare the two arrays and return the total distance between the two arrays at the same index using the reduce method
+  let totalDistance = 0
+  rightArray.forEach((right, index) => {
+    totalDistance += Math.abs(right - leftArray[index])
+  })
+
+
+  // for (let i = 0; i < rightArray.length; i++) {
+  //   totalDistance += Math.abs(rightArray[i] - leftArray[i])
+  // }
+
+
+  return totalDistance
 }
 
 console.log(this.partOne(TEST_DATA_PART_ONE))
--- input-types.js
+++ input-types.js
@@ -1,3 +1,6 @@
-export const TEST_DATA_PART_ONE = 'part-one-test-data.txt'
-export const TEST_DATA_PART_TWO = 'part-two-test-data.txt'
-export const INPUT = 'input.txt'
+exports = module.exports = {
+  TEST_DATA_PART_ONE: 'part-one-test-data.txt',
+  TEST_DATA_PART_TWO: 'part-two-test-data.txt',
+  INPUT: 'input.txt'
+}
+
